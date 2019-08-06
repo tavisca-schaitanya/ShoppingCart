fe1096f00a4353d1ddee0ea2f86b94a61e404981
tavisca-schaitanya <51919070+tavisca-schaitanya@users.noreply.github.com>
2019-08-06 12:32:04 +0530

    Delete Jean.cs

--- ShoppingCart/Jean.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShoppingCart
-{
-    public class Jean : Product
-    {
-        public override string ProductName { get; set; } = "jean";
-        public override double ProductPrice { get; set; } = 2599.0;
-        public override double Discount { get; set; } = 0;
-    }
-}
