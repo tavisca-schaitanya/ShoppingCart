c5ed7296854b0694dcdc961d3df7aa71ec463540
tavisca-schaitanya <51919070+tavisca-schaitanya@users.noreply.github.com>
2019-08-06 12:32:42 +0530

    Delete Shirt.cs

--- ShoppingCart/Shirt.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShoppingCart
-{
-    public class Shirt : Product
-    {
-        public override string ProductName { get; set; } = "shirt";
-        public override double ProductPrice { get; set; } = 1599.0;
-        public override double Discount { get; set; } = 0;
-    }
-}
